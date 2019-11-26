14c184954c19b44528fdcc352bd3f4517b3c47b2
Hitoishi Das <hitoishi.das@gmail.com>
2019-11-27 00:15:41 +0530

    wrkfmerkfn

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(db,{
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
   app.get('*',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'client','app','index.html'))
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 }
 app.use( '/', uplRoute );
